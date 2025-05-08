32a5527d0c071756ff30d35549e5d9b53295478a
Juan Marcelo Tisot <marcelotisot1990@gmail.com>
2025-05-07 21:15:11 -0300

    FIX CategoriesService imports (tests)

--- src/modules/products/products.controller.spec.ts
+++ src/modules/products/products.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { CategoriesService } from '../categories/categories.service';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
@@ -8,7 +9,10 @@ describe('ProductsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        CategoriesService
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
--- src/modules/products/products.service.spec.ts
+++ src/modules/products/products.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { CategoriesService } from '../categories/categories.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        CategoriesService
+      ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
