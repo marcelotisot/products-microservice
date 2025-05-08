ec0b62104610b518d9a4d9dfb2b6a7c4ca68e1c5
Juan Marcelo Tisot <marcelotisot1990@gmail.com>
2025-05-08 09:50:22 -0300

    se transforma a microservicio, se configura NATS

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { envs } from './config';
 import { PrismaExceptionFilter } from './common';
+import { envs } from './config';
 
 async function bootstrap() {
   const logger = new Logger('Main-ProductsMicroservice');
 
-  const app = await NestFactory.create(AppModule);
-
-  app.setGlobalPrefix('api');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers
+      }
+    }
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -20,7 +27,7 @@ async function bootstrap() {
 
   app.useGlobalFilters(new PrismaExceptionFilter);
 
-  await app.listen(envs.port);
+  await app.listen();
   
   logger.log(`Products Microservice running on port ${envs.port}`);
 }
